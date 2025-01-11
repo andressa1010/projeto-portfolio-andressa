98c318c107f6f0a41c19b8fcce86e1f2b8d2faba
andressa1010 <andressa.ns10@gmail.com>
2025-01-11 14:40:16 -0300

    alterar-css

--- src/Pages/contato.jsx
+++ src/Pages/contato.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Menu from "../components/menu";
 
-
 const Contato = () => {
-    return (
-        <>
-        <Menu/>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <Menu />
+
+      <div className="container-text">
+        <p>
+          Obrigada por visitar meu portfólio! Fico muito feliz em poder
+          compartilhar meus projetos e experiências Cada linha de código e cada
+          desafio enfrentado me trouxeram até aqui, e é com muito orgulho que
+          mostro um pouco do que sou capaz de criar. Se você gostou do meu
+          trabalho ou quer saber mais sobre como podemos colaborar, entre em
+          contato. Vamos construir algo incrível juntos!"
+        </p>
+        <img className="img-andressa" src="/2024092809560212.png"></img>
+      </div>
+
+     
+    </>
+  );
+};
+
 export default Contato;
