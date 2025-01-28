0d548b91e7d60806e90e2b0952f81175069563a2
andressa1010 <andressa.ns10@gmail.com>
2025-01-28 09:47:01 -0300

    zap-projetos

--- src/Pages/projetos.jsx
+++ src/Pages/projetos.jsx
@@ -168,6 +168,12 @@ const Projetos = () => {
           </div>
         ))}
       </div>
+
+      <div className="whatsapp">
+        <a href="https://wa.me/5511986414262" target="_blank">
+          <img src="/whatsapp.png"></img>
+        </a>
+      </div>
     </>
   );
 };
